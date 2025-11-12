defcec3abd999e8b156a765e57cfe510f4948aa9
villanelle3 <laura.vasconcelos@ufpe.br>
2025-11-12 17:27:25 -0300

    .........

---
+++ Tutorial05x05/sl_trees.jl
@@ -0,0 +1,23 @@
+################################################################################
+# ML > Supervised Learning > Trees
+################################################################################
+
+# load packages
+# import Pkg
+# Pkg.add("DecisionTree")
+
+using DecisionTree
+
+using Random, Statistics
+
+# load data
+
+X, y = load_data("iris")
+
+# view data types
+
+# view data types
+
+X
+
+y
